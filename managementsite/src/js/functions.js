1c370ddd662a4c8433bf08c0eab562da67c90b22
hasecom <hasetennisth@gmail.com>
2019-09-01 02:16:04 +0900

    pageUpdate

--- managementsite/src/js/functions.js
+++ managementsite/src/js/functions.js
@@ -2,6 +2,9 @@
 /// 何でも屋さん
 /// </summary>
 
+/// <summary>
+/// 日付のフォーマッター
+/// </summary>
  export function  DataFormat(date,type){
         var format = 
         {
@@ -14,3 +17,27 @@
         var val= tmp.join('')
         return val;
     }
+
+/// <summary>
+/// 連続するデータを分割する obj=値 , page=現在のページ , display=１ページで表示する件数
+/// </summary>
+export function Pages(obj,Nowpage,display){
+
+    var objLength = obj.length;  
+    if(display > objLength){
+       display = objLength;
+   }
+return obj.slice((Nowpage -1)*display,((Nowpage -1)*display)+display);
+
+}
+/// <summary>
+/// 何ページまで表示できるか
+/// </summary>
+export function NowPage(obj,display){
+    var objLength = obj.length;
+    var page = 1;
+    if(objLength > display){
+      page = Math.ceil(objLength/display);
+    }
+    return page;
+}
--- managementsite/src/scene/News.vue
+++ managementsite/src/scene/News.vue
@@ -1,15 +1,58 @@
 <template>
-    <div>
-        avkfokbdofkod<br>dfd
-        {{PageData}}
+<div>
+    <div class="card mt-2 mx-3" v-for="(val,index) in displayNewsFnc(News.Contents,nowpage,displaynews)" :key="index">
+        <div class="card-header row notMargin">
+            <span class="col-8 px-0">{{val.Title}}</span>
+            <span class="text-right col-4 news_date px-0">{{dataFormater(val.Date)}}</span>
+        </div>
+        <div class="card-body">
+            {{val.Content}}
+        </div>
     </div>
+    <div class="choreography mt-3">
+        <div class="d-inline-block border shadow-sm mx-1" v-for="n in howmanyPages(News.Contents,displaynews)">
+            <button class="btn btn-light" @click="nowpage = n">{{n}}</button>
+        </div>
+    </div>
+</div>
 </template>
+
 <script>
+import news from "@/data/news/NewsData.json";
+import * as fncs from "@/js/functions.js";
 export default {
-        props: {
+    props: {
         PageData: {
             type: Object
         }
     },
+    data() {
+        return {
+            News: news,
+            nowpage: 1,
+            displaynews: 3
+        }
+    },
+    methods: {
+        dataFormater(date) {
+            return fncs.DataFormat(date, 1);
+        },
+        displayNewsFnc(obj, page, display) {
+            return fncs.Pages(obj, page, display);
+        },
+        howmanyPages(obj, display) {
+            return fncs.NowPage(obj, display)
+        }
+    }
 }
 </script>
+
+<style scoped>
+.news_date {
+    font-size: 12px;
+}
+
+.choreography {
+    text-align: center;
+}
+</style>
