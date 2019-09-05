f50b1f9ad2c67bb6186b09020d16fffa9f0adbdc
hasecom <hasetennisth@gmail.com>
2019-09-05 23:51:30 +0900

    Update

--- managementsite/src/scene/Activity.vue
+++ managementsite/src/scene/Activity.vue
@@ -1,13 +1,14 @@
 <template>
-<div class="px-3">
-    <div v-for="(val,index) in Activity.Contents" :key="index" class="card bg-dark text-white mt-3 border shadow-lg activity_card">
-        <img class="card-img activity_img" :src="require('../'+ val.Image)" :alt="val.Title">
-        <div class="card-img-overlay activity_bck">
-            <h5 class="card-title row">
-                <span class="d-block col-10">{{val.Title}}</span>
-                <span class="d-block col-2 text-right"><i class="fas fa-chevron-right"></i></span>
-            </h5>
-            <p class="card-text text-right pr-4">{{val.Price}}</p>
+<div>
+    <div class="row notMargin" ref="activity_card">
+        <div v-for="(val,keys) in DisplayContent" :key="keys" class="col-6 activity_card mb-1" data-toggle="modal" data-target="#ActivityModal">
+            <div class="card"><modal :modalData="val"/>
+                <img class="activity_img" :src="require('../'+val.Image)" :style="img_height" alt="">
+                <div class="main_card">
+                    <div class="activity_title text-left px-1">{{val.Title}}</div>
+                    <p class="card-text text-right px-1"><small>{{val.Price}}</small></p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -15,33 +16,62 @@
 
 <script>
 import activity from "@/data/activity/Activity.json";
+import modal from "@/components/ActivityModal.vue";
 export default {
     props: {
         PageData: {
             type: Object
         }
     },
-    data(){
-        return{
-            Activity:activity
+    components: {
+        modal
+    },
+    data() {
+        return {
+            Activity: activity,
+            DisplayContent: activity.Contents,
+            ImgHeight: 0
+        }
+    },
+    mounted: function () {
+        this.ImgHeight = this.$refs.activity_card.clientWidth / 2;
+
+    },
+    computed: {
+        img_height() {
+            return "height:" + this.ImgHeight + 'px';
         }
     }
+
 }
 </script>
 
 <style scoped>
 .activity_img {
     width: 100%;
-    height: inherit;
+    height: 100px;
     object-fit: cover;
 }
-.activity_card{
-    width:100%; 
-    height:110px;
+
+.activity_card {
+    width: 100%;
+    padding-left: 3px;
+
+    padding-right: 3px;
+}
+
+.activity_title {
+    font-size: 15px;
+    height: 30px;
+
 }
 
 .activity_bck {
     background: rgba(30, 30, 30, 0.4)
+}
+
+.main_card {
+    height: 60px;
 }
 
 .activity_bck .card-title {
